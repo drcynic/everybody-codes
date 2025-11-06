dab16ebf6e63f036736f4ea6aa0f218380f971a9
cynic <daniel@szymanski.cc>
2025-11-06 17:39:42 +0100

    Simplify quest 03 part 1 + 2

--- events/2025/03/src/main.rs
+++ events/2025/03/src/main.rs
@@ -1,37 +1,16 @@
-use std::{
-    collections::{HashMap, HashSet},
-    iter::*,
-};
+use std::{collections::HashMap, iter::*};
 
 use itertools::Itertools;
 
 fn main() {
     let filename = "everybody_codes_e2025_q03_p1a.txt";
     let input = std::fs::read_to_string(filename).unwrap();
-    let r1 = input
-        .trim()
-        .split(',')
-        .fold(HashSet::new(), |mut acc, s| {
-            acc.insert(s.parse::<i64>().unwrap());
-            acc
-        })
-        .iter()
-        .sum::<i64>();
+    let r1 = input.trim().split(',').map(|s| s.parse::<i64>().unwrap()).unique().sum::<i64>();
     println!("Part1: {}", r1);
 
     let filename = "everybody_codes_e2025_q03_p2a.txt";
     let input = std::fs::read_to_string(filename).unwrap();
-    let r2 = input
-        .trim()
-        .split(',')
-        .fold(HashSet::new(), |mut acc, s| {
-            acc.insert(s.parse::<i64>().unwrap());
-            acc
-        })
-        .iter()
-        .sorted()
-        .take(20)
-        .sum::<i64>();
+    let r2 = input.trim().split(',').map(|s| s.parse::<i64>().unwrap()).sorted().dedup().take(20).sum::<i64>();
     println!("Part2: {}", r2);
 
     let filename = "everybody_codes_e2025_q03_p3a.txt";
