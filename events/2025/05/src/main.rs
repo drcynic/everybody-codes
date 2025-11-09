8be4318946f312c0ce88f289476232f63da40f44
cynic <daniel@szymanski.cc>
2025-11-09 12:37:15 +0100

    Remove own sword sort func for quest 05 part 3

--- events/2025/05/src/main.rs
+++ events/2025/05/src/main.rs
@@ -37,12 +37,13 @@ fn main() {
             let fishbone = parse_fishbone(r);
             let qual = quality(&fishbone);
             let levels = levels(&fishbone);
-            (id, qual, levels)
+            (qual, levels, id)
         })
-        .sorted_by(cmp_swords)
+        .sorted()
         .rev()
+        // .inspect(|e| println!("{:?}", e))
         .enumerate()
-        .map(|(idx, (id, _, _))| (idx as i64 + 1) * id)
+        .map(|(idx, (_, _, id))| (idx as i64 + 1) * id)
         .sum::<i64>();
     println!("Part3: {}", r3);
 }
@@ -75,33 +76,4 @@ fn levels(fishbone: &Vec<(Option<i64>, i64, Option<i64>)>) -> Vec<i64> {
         .iter()
         .map(|e| format!("{}{}{}", e.0.unwrap_or(0), e.1, if let Some(r) = e.2 { &r.to_string() } else { "" }).parse::<i64>().unwrap())
         .collect::<_>()
-}
-
-fn cmp_swords((l_id, l_qual, l_levels): &(i64, i64, Vec<i64>), (r_id, r_qual, r_levels): &(i64, i64, Vec<i64>)) -> Ordering {
-    if l_qual != r_qual {
-        return l_qual.cmp(r_qual); // sort by quality
-    }
-
-    // sort by levels
-    let mut idx = 0;
-    while idx < l_levels.len() && idx < r_levels.len() {
-        if l_levels[idx] < r_levels[idx] {
-            return Ordering::Less;
-        } else if l_levels[idx] > r_levels[idx] {
-            return Ordering::Greater;
-        }
-        idx += 1;
-    }
-    if idx == l_levels.len() && idx == r_levels.len() {
-        // sort by id
-        if l_id > r_id {
-            return Ordering::Greater;
-        } else {
-            return Ordering::Less;
-        }
-    } else if idx == l_levels.len() {
-        Ordering::Less
-    } else {
-        Ordering::Greater
-    }
 }
