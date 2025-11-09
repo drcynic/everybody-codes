872516352ada94db1d96d765ab1e0dd7a1ab3a74
cynic <daniel@szymanski.cc>
2025-11-09 14:01:50 +0100

    Simplify quest 03 part 3

--- events/2025/03/src/main.rs
+++ events/2025/03/src/main.rs
@@ -1,29 +1,15 @@
-use std::{collections::HashMap, iter::*};
-
 use itertools::Itertools;
 
 fn main() {
-    let filename = "everybody_codes_e2025_q03_p1a.txt";
-    let input = std::fs::read_to_string(filename).unwrap();
+    let input = std::fs::read_to_string("everybody_codes_e2025_q03_p1a.txt").unwrap();
     let r1 = input.trim().split(',').map(|s| s.parse::<i64>().unwrap()).unique().sum::<i64>();
     println!("Part1: {}", r1);
 
-    let filename = "everybody_codes_e2025_q03_p2a.txt";
-    let input = std::fs::read_to_string(filename).unwrap();
-    let r2 = input.trim().split(',').map(|s| s.parse::<i64>().unwrap()).sorted().dedup().take(20).sum::<i64>();
+    let input = std::fs::read_to_string("everybody_codes_e2025_q03_p2a.txt").unwrap();
+    let r2 = input.trim().split(',').map(|s| s.parse::<i64>().unwrap()).sorted_unstable().dedup().take(20).sum::<i64>();
     println!("Part2: {}", r2);
 
-    let filename = "everybody_codes_e2025_q03_p3a.txt";
-    let input = std::fs::read_to_string(filename).unwrap();
-    let r3 = *input
-        .trim()
-        .split(',')
-        .fold(HashMap::new(), |mut acc, s| {
-            *acc.entry(s.parse::<i64>().unwrap()).or_insert(0) += 1;
-            acc
-        })
-        .values()
-        .max()
-        .unwrap();
+    let input = std::fs::read_to_string("everybody_codes_e2025_q03_p3a.txt").unwrap();
+    let r3 = input.trim().split(',').sorted_unstable().dedup_with_count().map(|(c, _)| c).max().unwrap();
     println!("Part3: {}", r3);
 }
