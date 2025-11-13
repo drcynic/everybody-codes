1e222b40a748d1160db12fd932b05be014d763a7
cynic <daniel@szymanski.cc>
2025-11-13 09:25:43 +0100

    Simplify quest 07 and use DP with memoization for part 3

--- events/2025/07/src/main.rs
+++ events/2025/07/src/main.rs
@@ -1,6 +1,7 @@
 use std::collections::{HashMap, HashSet};
 
 use itertools::Itertools;
+use memoize::memoize;
 
 fn main() {
     //p1
@@ -12,31 +13,26 @@ fn main() {
     //p2
     let input = std::fs::read_to_string("everybody_codes_e2025_q07_p2a.txt").unwrap();
     let (names, rules) = names_and_rules(&input);
-    let filtered = names
-        .iter()
-        .enumerate()
-        .filter(|(_, n)| n.as_bytes().iter().tuple_windows().all(|(c, n)| rules.get(c).unwrap().contains(n)))
-        .map(|(idx, _)| idx + 1)
-        .sum::<usize>();
+    let filtered = names.iter().enumerate().filter_map(|(idx, n)| check_name(&rules, n).then(|| idx + 1)).sum::<usize>();
     println!("Part2: {:?}", filtered);
 
     let input = std::fs::read_to_string("everybody_codes_e2025_q07_p3a.txt").unwrap();
     let (names, rules) = names_and_rules(&input);
     let prefixes = filter_names(&names, &rules);
-    let mut unique_names = HashSet::new();
-    for p in &prefixes {
-        gen_names(p, &rules, &mut unique_names);
-    }
-    println!("Part3: {:?}", unique_names.len());
+    let p3 = prefixes
+        .iter()
+        .filter(|p| prefixes.iter().all(|q| *p == q || !p.starts_with(q)))
+        .map(|p| gen_names(*p.as_bytes().last().unwrap(), p.len(), &rules))
+        .sum::<usize>();
+    println!("Part3: {:?}", p3);
+}
+
+fn check_name(rules: &HashMap<u8, HashSet<u8>>, n: &str) -> bool {
+    n.as_bytes().iter().tuple_windows().all(|(c, n)| rules.get(c).unwrap().contains(n))
 }
 
 fn filter_names(names: &Vec<String>, rules: &HashMap<u8, HashSet<u8>>) -> Vec<String> {
-    let filtered = names
-        .iter()
-        .filter(|n| n.as_bytes().iter().tuple_windows().all(|(c, n)| rules.get(c).unwrap().contains(n)))
-        .map(|s| s.to_string())
-        .collect::<Vec<_>>();
-    filtered
+    names.iter().filter_map(|n| check_name(&rules, n).then(|| n.to_string())).collect::<Vec<_>>()
 }
 
 fn names_and_rules(input: &str) -> (Vec<String>, HashMap<u8, HashSet<u8>>) {
@@ -44,29 +40,22 @@ fn names_and_rules(input: &str) -> (Vec<String>, HashMap<u8, HashSet<u8>>) {
     let names = names.trim().split(",").map(|s| s.to_string()).collect::<Vec<_>>();
     let rules = rules.trim().lines().fold(HashMap::new(), |mut acc, l| {
         let (c, f) = l.split_once(" > ").unwrap();
-        let f = f.split(",").map(|s| *s.trim().as_bytes().first().unwrap()).collect::<Vec<_>>();
-        let f: HashSet<u8> = HashSet::from_iter(f);
+        let f: HashSet<u8> = f.split(",").map(|s| *s.trim().as_bytes().first().unwrap()).collect();
         acc.insert(*c.as_bytes().first().unwrap(), f);
         acc
     });
     (names, rules)
 }
 
-fn gen_names(prefix: &str, rules: &HashMap<u8, HashSet<u8>>, unique_names: &mut HashSet<String>) {
-    let mut stack = vec![prefix.to_string()];
-
-    while let Some(name) = stack.pop() {
-        let last_char = name.as_bytes().last().unwrap();
-        if let Some(possibilities) = rules.get(last_char) {
-            for possibility in possibilities {
-                let new_name = format!("{}{}", name, *possibility as char);
-                if new_name.len() <= 11 {
-                    stack.push(new_name.clone());
-                    if new_name.len() >= 7 {
-                        unique_names.insert(new_name);
-                    }
-                }
-            }
-        }
+#[memoize(Ignore: rules)]
+fn gen_names(letter: u8, size: usize, rules: &HashMap<u8, HashSet<u8>>) -> usize {
+    if size == 11 {
+        return 1;
+    }
+    let count = if size < 7 { 0 } else { 1 };
+    if let Some(followers) = rules.get(&letter) {
+        count + followers.iter().map(|f| gen_names(*f, size + 1, rules)).sum::<usize>()
+    } else {
+        count
     }
 }
